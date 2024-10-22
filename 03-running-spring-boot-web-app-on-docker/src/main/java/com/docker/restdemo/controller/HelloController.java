a9dd928f903f77b4d438aace801ff652535accbd
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-10-22 21:47:38 +0700

    add: 03-running-spring-boot-web-app-on-docker
    Running Spring Boot Web App On Docker

---
+++ 03-running-spring-boot-web-app-on-docker/src/main/java/com/docker/restdemo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.docker.restdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
